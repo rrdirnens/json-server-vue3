c11de89156630c8e49a274695fe7f7f1a9034fce
Rob Dirnens <42729985+rrdirnens@users.noreply.github.com>
2021-02-12 10:44:37 +0200

    Init with working server

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,36 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>TODOS:</h1>
+  <ul>
+    <li v-for="user of users" :key="user.id">
+      {{user.name}}
+    </li>
+  </ul>
 </template>
 
 <script>
+import axios from 'axios'
 import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
     HelloWorld
+  },
+  data () {
+    return {
+      users: []
+    }
+  },
+  async created () {
+    try {
+      const res = await axios.get('http://localhost:3000/users')
+
+      this.users = res.data
+    } catch (e) {
+      console.error(e)
+    }
   }
 }
 </script>
