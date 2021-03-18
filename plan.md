aaaae27b62979bf7fcde29a855e3cee30f2ecd4f
Rob Dirnens <42729985+rrdirnens@users.noreply.github.com>
2021-03-18 11:24:22 +0200

    Add routing

--- plan.md
+++ plan.md
@@ -1,8 +1,11 @@
 ## Steps
 
-1: Add routing (Home -> reg/login -> dashboard)
-2: Add first binary metric to track manually (counter++ on every action)
-3: Create a way of storing the metric in the database to the logged in user
-4: Prevent logged out users from tracking the metric
-5: Show only tracked metrics data specific to logged in user
+[DONE] 
+- Add routing (Home -> reg/login -> dashboard)
+
+
+[TO-BE-DONE]
+- Remove and store all authentication things(don't actually delete). Develop the interesting parts (tracking and displaying metrics), not authentication shit, which is a backend thing anyway.
+- Add first binary metric to track manually (counter++ on every action). The metric being track will be determined by user input: the user adds the name of the metric and determins what each unit represents. For example, the user clicks the button every time he has smoked a pack of cigarettes. 
+- Create a way of storing the metric in the database
 
--- src/App.vue
+++ src/App.vue
@@ -1,167 +1,18 @@
 <template>
-  <HelloWorld msg="Private tracking" />
-  <div style="display: none" class="create-new-user" @click="addUser">
-    REGISTER
+<!-- <HelloWorld msg="Private tracking" /> -->
+
+  <div id="nav">
+    <router-link to="/">Home</router-link>
+    <router-link to="/about">About</router-link>
   </div>
 
-
-
-
-
-  <div class="register-user">
-    <div class="title">Register, please:</div>
-    <input
-      type="text"
-      class="user-name"
-      v-model="registration.user"
-      placeholder="Username"
-    />
-    <input
-      type="password"
-      class="user-password"
-      v-model="registration.pw"
-      placeholder="Password"
-    />
-    <button class="reg-submit" @click="registerUser">Register</button>
-  </div>
-
-
-  <div class="login-user" style="margin-top: 20px;">
-    <div class="title">Log in, please:</div>
-    <input
-      type="text"
-      class="user-name"
-      v-model="login.user"
-      placeholder="Username"
-    />
-    <input
-      type="password"
-      class="user-password"
-      v-model="login.pw"
-      placeholder="Password"
-    />
-    <button class="reg-submit" @click="loginUser">Login</button>
-  </div>
-
-
-
-
-
-  <div style="">
-    <h1>Todos:</h1>
-    <div class="form-wrap">
-      <label for="new-user">Enter new todo: </label>
-      <input type="text" name="new-user" v-model="todoName" />
-      <button class="submit" @click="addTodo">Submit</button>
-    </div>
-  </div>
-
-  <ul style="">
-    <li v-for="todo of todos" :key="todo.id">
-      {{ todo.todo }}
-    </li>
-  </ul>
-
-
+  <router-view/>
 
 </template>
 
-<script>
-import axios from "axios";
-import HelloWorld from "./components/HelloWorld.vue";
+ <script>
 
-const baseURL = "http://localhost:3000/todos";
-const loginURL = "http://localhost:3000/auth/login";
-const registerURL = "http://localhost:3000/auth/register";
-
-export default {
-  name: "App",
-  components: {
-    HelloWorld,
-  },
-  data() {
-    return {
-      todos: [],
-      todoName: '',
-      accessToken: '',
-      registration: {
-        user: '',
-        pw: '',
-      },
-      login: {
-        user: '',
-        pw: ''
-      }
-    };
-  },
-  async created() {},
-  methods: {
-    async addTodo() {
-      const res = await axios.post(baseURL, {
-        todo: this.todoName,
-      });
-      console.log(res.data);
-      this.todos = [...this.todos, res.data];
-      this.todoName = "";
-    },
-
-    async registerUser() {
-      try {
-        const res = await axios({
-          method: "post",
-          url: registerURL,
-          data: {
-            email: this.registration.user,
-            password: this.registration.pw,
-          },
-        });
-        this.accessToken = res.data;
-        console.log(JSON.stringify(this.accessToken));
-        console.log("Registration successful");
-        this.registration.user = ''
-        this.registration.pw = ''
-      } catch (e) {
-        console.error(e);
-      }
-    },
-
-    async loginUser() {
-      try {
-        const res = await axios({
-          method: "post",
-          url: loginURL,
-          data: {
-            email: this.login.user,
-            password: this.login.pw,
-          },
-        });
-
-        this.accessToken = res.data;
-        console.log(JSON.stringify(this.accessToken));
-        console.log("Login successful");
-        this.getTodos();
-      } catch (e) {
-        console.error(e);
-      }
-    },
-    async getTodos() {
-      await axios
-        .get(baseURL, {
-          headers: {
-            Authorization: `Bearer ${this.accessToken}`,
-          },
-        })
-        .then((res) => {
-          console.log(res.data);
-          this.todos = res.data;
-        })
-        .catch((error) => {
-          console.error(error);
-        });
-    },
-  },
-};
-</script>
+ </script>
 
 <style lang="scss">
 #app {
